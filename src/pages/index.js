99b2a760904dc91fdc0d57faadcce5f79a1df3a2
joelbitar <joel.shapiro@divido.com>
2019-11-25 07:01:26 +0100

    Adding layout and ignoring .idea (#1)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,20 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Joel Shapiro" />
+    <h1>Joel Shapiro</h1>
+    <p>Min hemsida</p>
+    <p>Testar automatiskt bygge med circleci pushat till firebase</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+  </Layout>
+)
+
+export default IndexPage
